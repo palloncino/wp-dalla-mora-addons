e3c72aa94bc025030f9e85d65d4ce0f9a7da07ea
palloncino <antonio.guiotto.dev@gmail.com>
2024-02-20 14:38:37 +0100

    responsive footer and homepage

--- Homepage Backdrops (Cover 1 and 2).js
+++ Homepage Backdrops (Cover 1 and 2).js
@@ -1,16 +1,29 @@
 window.addEventListener("DOMContentLoaded", function () {
-  const coverSection1 = document.querySelector(".homepage-cover-1");
+  
+  // Create backdrop dark
   const backdrop1_dark = document.createElement("div");
-  if (coverSection1) {
-    backdrop1_dark.classList.add("backdrop", "backdrop--dark");
-    coverSection1.appendChild(backdrop1_dark);
-  }
-  const coverSection2 = document.querySelector(".homepage-cover-2");
-	console.log(1, {coverSection2})
+  backdrop1_dark.classList.add("backdrop", "backdrop--dark");
+
+  // Create backdrop light
   const backdrop2_light = document.createElement("div");
-  if (coverSection2) {
-	  	console.log(2, {backdrop2_light, coverSection2})
-    backdrop2_light.classList.add("backdrop", "backdrop--light");
-    coverSection2.appendChild(backdrop2_light);
+  backdrop2_light.classList.add("backdrop", "backdrop--light");
+
+  // Select backdrop targets
+  const homepage_cover_1 = document.querySelector(".homepage-cover-1");
+  const homepage_cover_2 = document.querySelector(".homepage-cover-2");
+  const servizi_cover_1 = document.querySelector(".servizi-cover-1");
+
+  // Apply backdrops to covers (full screen backgrounds)
+
+  if (homepage_cover_1) {
+    homepage_cover_1.appendChild(backdrop1_dark);
+  }
+
+  if (homepage_cover_2) {
+    homepage_cover_2.appendChild(backdrop2_light);
+  }
+
+  if (servizi_cover_1) {
+    servizi_cover_1.appendChild(backdrop1_dark);
   }
 });
