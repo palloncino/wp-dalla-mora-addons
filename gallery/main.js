82dcb58d11354817dc5e53ef7c3b680104fdb1d3
palloncino <antonio.guiotto.dev@gmail.com>
2024-03-04 13:55:04 +0100

    style

--- gallery/main.js
+++ gallery/main.js
@@ -1,20 +1,52 @@
 const array = [
-  "./video-1.mp4",
-  "http://www.gruppodallamora.it/wp-content/uploads/2024/02/cover-civile-1536x570.png",
-  "http://www.gruppodallamora.it/wp-content/uploads/2024/02/cave-1024x768.jpeg",
-  "./video-2.mp4",
-  "http://www.gruppodallamora.it/wp-content/uploads/2024/02/cave-1536x1152.jpeg",
-  "./video-1.mp4",
-  "http://www.gruppodallamora.it/wp-content/uploads/2024/02/cover-civile-1536x570.png",
-  "http://www.gruppodallamora.it/wp-content/uploads/2024/02/cave-1024x768.jpeg",
-  "./video-2.mp4",
-  "http://www.gruppodallamora.it/wp-content/uploads/2024/02/cave-1536x1152.jpeg",
+  // Images
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-1.jpg",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-2-1.jpg",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-3-1.jpg",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-4.jpeg",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-5-1.jpg",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-6.jpg",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-7.jpg",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-8-1.jpg",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-9.jpg",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-10.jpg",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-11.jpg",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-12.jpg",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-13.jpg",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-14.jpg",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-15.jpg",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-16-1.jpg",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-17.jpg",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-18.jpg",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-19.jpg",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-20.jpg",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-21.jpg",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-22.jpg",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-23.png",
+  // Videos
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-1.mp4",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-2.mp4",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-3.mp4",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-4.mp4",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-5.mp4",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-6.mp4",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-7.mp4",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-8.mp4",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-9.mp4",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-10.mp4",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-11.mp4",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-12.mp4",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-13.mp4",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-14.mp4",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-15.mp4",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-16.mp4",
+  "http://www.gruppodallamora.it/wp-content/uploads/2024/03/lavori-17.mp4",
 ];
 
 document.addEventListener("DOMContentLoaded", function () {
-	if (!document.body.classList.contains('page-id-235')) {
-		return;
-	}
+  if (!document.body.classList.contains("page-id-235")) {
+    return;
+  }
   const galleryGridContainer = document.querySelector(".gallery-grid-container");
   const previewContainer = document.querySelector(".preview-container");
   const previewContent = document.querySelector(".preview-content");
@@ -39,7 +71,8 @@ document.addEventListener("DOMContentLoaded", function () {
       // Create overlay element
       const overlay = document.createElement("div");
       overlay.classList.add("overlay");
-      overlay.innerHTML = '<img height="50" src="http://www.gruppodallamora.it/wp-content/uploads/2024/02/video_11383324.png" alt="video" />';
+      overlay.innerHTML =
+        '<img height="50" src="http://www.gruppodallamora.it/wp-content/uploads/2024/03/play_14441317.png" alt="Play Video" />';
       imgBox.appendChild(overlay);
     } else {
       imgBox.style.backgroundImage = `url(${url})`; // Set image
@@ -50,16 +83,20 @@ document.addEventListener("DOMContentLoaded", function () {
     galleryGridContainer.appendChild(imgBox);
 
     imgBox.addEventListener("click", function () {
-      if (url.endsWith(".mp4")) {
-        previewContent.innerHTML = `<video controls autoplay style="width: 100%;"><source src="${url}" type="video/mp4"></video>`;
+      // Check if the URL is for an MP4 or MOV video
+      if (url.endsWith(".mp4") || url.endsWith(".mov")) {
+        // Assuming modern browsers that can play .mov files with 'video/mp4' MIME type
+        // If precise MIME type is needed, add a condition to switch between 'video/mp4' and 'video/quicktime'
+        const videoType = url.endsWith(".mov") ? "video/quicktime" : "video/mp4";
+        previewContent.innerHTML = `<video controls autoplay style="width: 100%;"><source src="${url}" type="${videoType}"></video>`;
       } else {
+        // Handle images
         previewContent.innerHTML = `<img src="${url}" style="max-width: 100%; max-height: 100%;">`;
       }
       previewContainer.style.display = "block";
       backdrop.style.display = "block";
     });
-});
-
+  });
 
   // Close preview when clicking backdrop
   backdrop.addEventListener("click", function () {
